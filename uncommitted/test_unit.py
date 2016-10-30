866f02fb7fa0ac6ec891043dcbfd4d560b9e0049
Brandon Rhodes <brandon@rhodesmill.org>
2016-10-30 12:07:59 -0400

    Simplify test of #16

--- uncommitted/test_unit.py
+++ uncommitted/test_unit.py
@@ -1,13 +1,6 @@
-# -*- coding: utf-8 -*-
-
-"""Test `uncommitted`'s implementation."""
-
-import mock
 import uncommitted.command
 
-def test_run_chars_outside_default_charset():
-    """Is `run` able to parse outputs containing chars outside the default charset?"""
-    with mock.patch('uncommitted.command.check_output') as mock_check_output:
-        mock_check_output.return_value = b'tsch\xfc\xdf' # 'tschüß' in latin1, outside UTF-8
-        # No crash, unknown chars are converted to question marks:
-        assert uncommitted.command.run(None) == [u'tsch\ufffd\ufffd'] # 'tsch??'
+def test_run_can_handle_badly_encoded_output():
+    some_bytes = b'tsch\xfc\xdf' # 'tschüß' in latin1, outside UTF-8
+    output = uncommitted.command.run(['echo', some_bytes])
+    assert output == [u'tsch\ufffd\ufffd']
